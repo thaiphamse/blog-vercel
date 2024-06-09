93bf016a9a41518bd8696e0f82762e79d3474d30
ThaiPhamSE <pqthai2909@gmail.com>
2024-06-09 21:28:20 +0700

    fix: comment connect db

--- src/api/v1/configs/mongoDB.js
+++ src/api/v1/configs/mongoDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 module.exports = {
     connect: async () => {
         try {
-            await mongoose.connect('mongodb://127.0.0.1:27017/test');
+            // await mongoose.connect('mongodb://127.0.0.1:27017/test');
             console.log("Successfully connect to mongo db");
         } catch (error) {
             handleError(error);
