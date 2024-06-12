06ef5910782969cd25530cd649522dcf99507044
ThaiPhamSE <pqthai2909@gmail.com>
2024-06-12 15:58:07 +0700

    refactor (connect db): Thêm biến môi trường kết nối db
    update biến môi trường và gitignore

--- src/api/v1/configs/mongoDB.js
+++ src/api/v1/configs/mongoDB.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 module.exports = {
     connect: async () => {
         try {
-            await mongoose.connect('mongodb+srv://thaipham12:Xtera123@cluster0.znj9b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+            console.log(process.env.DB_USERNAME);
+            const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.znj9b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+            await mongoose.connect(uri);
             console.log("Successfully connect to mongo db");
         } catch (error) {
             console.log(error);
